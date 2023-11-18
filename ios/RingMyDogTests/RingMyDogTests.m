f7d60c1dc53990eb9e26489b357c65fca6da6741
Akshat Sharma <sharmaakshat775@gmail.com>
2023-11-18 13:28:55 +0530

    app name changed

--- ios/RingMyDogTests/RingMyDogTests.m
+++ ios/RingMyDogTests/RingMyDogTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface DummyProjectTests : XCTestCase
+@interface RingMyDogTests : XCTestCase
 
 @end
 
-@implementation DummyProjectTests
+@implementation RingMyDogTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
